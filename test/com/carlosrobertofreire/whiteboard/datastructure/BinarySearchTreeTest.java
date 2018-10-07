01e5a62e89591390c85632226ec9eaf4828b93ae
Carlos Roberto Freire <carlosrobertofreire@gmail.com>
2018-10-07 16:25:25 -0300

    Add tests for printInOrder, printPreOrder and printPostOrder methods.

--- test/com/carlosrobertofreire/whiteboard/datastructure/BinarySearchTreeTest.java
+++ test/com/carlosrobertofreire/whiteboard/datastructure/BinarySearchTreeTest.java
@@ -1,16 +1,34 @@
 package com.carlosrobertofreire.whiteboard.datastructure;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class BinarySearchTreeTest {
-	
+
 	BinarySearchTree target = null;
+
+	private PrintStream sysOut;
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+	@Before
+	public void setUpStreams() {
+		sysOut = System.out;
+		System.setOut(new PrintStream(outContent));
+	}
 
 	@Before
 	public void setUp() throws Exception {
 		target = new BinarySearchTree();
+	}
+
+	@After
+	public void revertStreams() {
+		System.setOut(sysOut);
 	}
 
 	@Test
@@ -19,9 +37,9 @@ public class BinarySearchTreeTest {
 		Assert.assertEquals(true, target.contains(50));
 		target.insert(40);
 		Assert.assertEquals(true, target.contains(40));
-		Assert.assertEquals(false, target.contains(10));		
+		Assert.assertEquals(false, target.contains(10));
 	}
-	
+
 	@Test
 	public void test_find_success() {
 		target.insert(50);
@@ -31,14 +49,14 @@ public class BinarySearchTreeTest {
 		Assert.assertEquals(30, target.find(30).data);
 		Assert.assertEquals(10, target.find(10).data);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void test_find_inputDoesNotExist() {
 		target.insert(55);
 		target.insert(60);
 		target.find(30);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void test_find_targetIsEmpty() {
 		target.find(30);
@@ -48,7 +66,7 @@ public class BinarySearchTreeTest {
 	public void test_delete_inputDoesNotExist() {
 		target.delete(30);
 	}
-	
+
 	@Test
 	public void test_delete_success() {
 		target.insert(30);
@@ -60,18 +78,18 @@ public class BinarySearchTreeTest {
 		Assert.assertEquals(false, target.contains(30));
 		Assert.assertEquals(true, target.contains(10));
 	}
-	
+
 	@Test
 	public void test_contains_inputDoesNotExist() {
 		target.insert(45);
 		Assert.assertEquals(false, target.contains(10));
 	}
-	
+
 	@Test
 	public void test_contains_targetIsEmpty() {
 		Assert.assertEquals(false, target.contains(10));
 	}
-	
+
 	@Test
 	public void test_contains_success() {
 		target.insert(12);
@@ -81,4 +99,35 @@ public class BinarySearchTreeTest {
 		Assert.assertEquals(false, target.contains(10));
 	}
 
+	@Test
+	public void test_printInOrder_success() {
+		target.insert(30);
+		target.insert(20);
+		target.insert(18);
+		target.insert(40);
+		target.printInOrder();
+		Assert.assertEquals("18 20 30 40 ", outContent.toString());
+	}
+
+	@Test
+	public void test_printPreOrder_success() {
+		target.insert(30);
+		target.insert(20);
+		target.insert(18);
+		target.insert(40);
+		target.printPreOrder();
+		Assert.assertEquals("30 20 18 40 ", outContent.toString());
+
+	}
+
+	@Test
+	public void test_printPostOrder_success() {
+		target.insert(30);
+		target.insert(20);
+		target.insert(18);
+		target.insert(40);
+		target.printPostOrder();
+		Assert.assertEquals("18 20 40 30 ", outContent.toString());
+
+	}
 }
