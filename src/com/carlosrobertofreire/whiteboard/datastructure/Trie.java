c93b3810ab2942688bbfdf261d2271037886d9a2
Carlos Roberto Freire <carlosrobertofreire@gmail.com>
2018-11-16 06:04:56 -0200

    Add methods contains and delete for Trie data structure. (still under development)

--- src/com/carlosrobertofreire/whiteboard/datastructure/Trie.java
+++ src/com/carlosrobertofreire/whiteboard/datastructure/Trie.java
@@ -9,7 +9,6 @@ public class Trie {
 	private static int ALPHABET_SIZE = 26;
 
 	class TrieNode {
-
 		TrieNode[] children;
 		boolean isCompleteWord;
 
@@ -17,7 +16,6 @@ public class Trie {
 			children = new TrieNode[ALPHABET_SIZE];
 			isCompleteWord = false;
 		}
-
 	}
 
 	private TrieNode root;
@@ -29,17 +27,80 @@ public class Trie {
 		if (root == null) {
 			root = new TrieNode();
 		}
-		TrieNode current = root;
+		TrieNode currentTrieNode = root;
 		int index = 0;
 		for (int level = 0; level < word.length(); level++) {
 			char letter = word.charAt(level);
 			index = letter - 'a';
-			if (current.children[index] == null) {
-				current.children[index] = new TrieNode();
+			if (currentTrieNode.children[index] == null) {
+				currentTrieNode.children[index] = new TrieNode();
 			}
-			current = current.children[index];
+			currentTrieNode = currentTrieNode.children[index];
 		}
-		current.children[index].isCompleteWord = true;
+		currentTrieNode.children[index].isCompleteWord = true;
+	}
+
+	public boolean contains(String word) {
+		if (isEmpty() || !isValid(word)) {
+			return false;
+		}
+		TrieNode currentTrieNode = root;
+		for (int level = 0; level < word.length(); level++) {
+			char letter = word.charAt(level);
+			int index = letter - 'a';
+			if (currentTrieNode.children[index] == null) {
+				return false;
+			}
+			currentTrieNode = currentTrieNode.children[index];
+		}
+		return currentTrieNode.isCompleteWord;
+	}
+
+	public void delete(String word) {
+		if (isEmpty()) {
+			throw new IllegalStateException("Trie is empty!");
+		}
+		if (word == null || word.isEmpty()) {
+			throw new IllegalArgumentException("String is invalid!");
+		}
+		delete(root, word, 0);
+	}
+
+	private void delete(TrieNode current, String word, int position) {
+		if (position >= word.length()) {
+			return;
+		}
+		int index = word.charAt(position) - 'a';
+		if (current.children[index] == null) {
+			throw new IllegalArgumentException("Word does not exist in the Trie!");
+		} else {
+			delete(current.children[index], word, position++);
+		}
+		if (position == (word.length() - 1)) {
+			if (isLeaf(current)) {
+				current = null;
+			} else {
+				current.isCompleteWord = false;
+			}
+		} else {
+			if (current.isCompleteWord) {
+				return;
+			}
+			if (isLeaf(current)) {
+				current = null;
+			}
+		}
+	}
+
+	private boolean isLeaf(TrieNode node) {
+		if (node == null)
+			throw new IllegalArgumentException("Node is null!");
+		for (int i = 0; i < node.children.length; i++) {
+			if (node.children[i] != null) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
