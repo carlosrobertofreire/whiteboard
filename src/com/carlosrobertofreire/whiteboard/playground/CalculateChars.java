adfb68fc3f7d27f2fd9a306c1f9444c8dc53e5db
Carlos Roberto Freire <carlosrobertofreire@gmail.com>
2018-11-19 06:24:45 -0200

    Add more chars to CalculateChars.

--- src/com/carlosrobertofreire/whiteboard/playground/CalculateChars.java
+++ src/com/carlosrobertofreire/whiteboard/playground/CalculateChars.java
@@ -3,8 +3,8 @@ package com.carlosrobertofreire.whiteboard.playground;
 public class CalculateChars {
 
 	public static void main(String[] args) {
-		String word = "Abcdefg";
-		char[] letters = word.toLowerCase().toCharArray();
+		String word = "A ãé bcd efg wkyz";
+		char[] letters = word.toCharArray();
 		for (char letter : letters) {
 			int index = letter - 'a';			
 			System.out.println("Result for letter " + letter + ": " + index); 
