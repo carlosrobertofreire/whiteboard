63c1ffc29a631ff37bcb0251105747243d3a3531
Carlos Roberto Freire <carlosrobertofreire@gmail.com>
2018-12-20 15:00:22 -0200

    Add Test Suites.

---
+++ test/com/carlosrobertofreire/whiteboard/AllTests.java
@@ -0,0 +1,13 @@
+package com.carlosrobertofreire.whiteboard;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.carlosrobertofreire.whiteboard.datastructure.AllDataStructureTests;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AllDataStructureTests.class })
+public class AllTests {
+
+}
---
+++ test/com/carlosrobertofreire/whiteboard/datastructure/AllDataStructureTests.java
@@ -0,0 +1,11 @@
+package com.carlosrobertofreire.whiteboard.datastructure;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ BinarySearchTreeTest.class, DynamicArrayTest.class, GraphTest.class, HashtableTest.class, LinkedListTest.class, MinHeapTest.class, QueueTest.class, StackTest.class, TrieTest.class })
+public class AllDataStructureTests {
+
+}
