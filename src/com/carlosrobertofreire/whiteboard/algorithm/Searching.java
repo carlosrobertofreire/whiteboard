414e7bf64835d7d49f7e577818711f716274888b
Carlos Roberto Freire <carlosrobertofreire@gmail.com>
2018-12-28 18:31:29 -0200

    Add searching algorithm: Binary Search.

---
+++ src/com/carlosrobertofreire/whiteboard/algorithm/Searching.java
@@ -0,0 +1,55 @@
+package com.carlosrobertofreire.whiteboard.algorithm;
+
+/**
+ * @author carlosrobertofreire
+ *
+ */
+public class Searching {
+
+	/**
+	 * O(log n) - Logarithmic
+	 */
+	public static boolean binarySearchRecursive(int[] array, int key) {
+		if (array == null || array.length == 0) {
+			throw new IllegalArgumentException("Array must have values!");
+		}
+		return binarySearchRecursive(array, key, 0, array.length - 1);
+	}
+
+	private static boolean binarySearchRecursive(int[] array, int key, int startIndex, int endIndex) {
+		if (startIndex > endIndex) {
+			return false;
+		}
+		int middleIndex = (startIndex + endIndex) / 2;
+		if (array[middleIndex] > key) {
+			return binarySearchRecursive(array, key, startIndex, middleIndex - 1);
+		} else if (array[middleIndex] < key) {
+			return binarySearchRecursive(array, key, middleIndex + 1, endIndex);
+		} else {
+			return true;
+		}
+	}
+
+	/**
+	 * O(log n) - Logarithmic
+	 */
+	public static boolean binarySearchIterative(int[] array, int key) {
+		if (array == null || array.length == 0) {
+			throw new IllegalArgumentException("Array must have values!");
+		}
+		int startIndex = 0;
+		int endIndex = array.length - 1;
+		while (startIndex <= endIndex) {
+			int middleIndex = (startIndex + endIndex) / 2;
+			if (array[middleIndex] > key) {
+				endIndex = middleIndex - 1;
+			} else if (array[middleIndex] < key) {
+				startIndex = middleIndex + 1;
+			} else {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
