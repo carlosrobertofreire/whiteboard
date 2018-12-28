ccdb7318e6f23a0d462cdc9a63434270b6b43c70
Carlos Roberto Freire <carlosrobertofreire@gmail.com>
2018-12-28 18:59:31 -0200

    Add new test suite for algorithm package.

--- test/com/carlosrobertofreire/whiteboard/AllTests.java
+++ test/com/carlosrobertofreire/whiteboard/AllTests.java
@@ -4,10 +4,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.carlosrobertofreire.whiteboard.algorithm.AllAlgorithmTests;
 import com.carlosrobertofreire.whiteboard.datastructure.AllDataStructureTests;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AllDataStructureTests.class })
+@SuiteClasses({ AllAlgorithmTests.class, AllDataStructureTests.class })
 public class AllTests {
 
 }
