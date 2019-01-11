1ba699f94ae6aa5f3be4e0709533bce7cdd014ea
Carlos Roberto Freire <carlosrobertofreire@gmail.com>
2019-01-11 06:59:14 -0200

    Add tests for Quick Sort algorithm.

--- test/com/carlosrobertofreire/whiteboard/algorithm/AllAlgorithmTests.java
+++ test/com/carlosrobertofreire/whiteboard/algorithm/AllAlgorithmTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ BinarySearchTest.class, MergeSortTest.class })
+@SuiteClasses({ BinarySearchTest.class, MergeSortTest.class, QuickSortTest.class })
 public class AllAlgorithmTests {
 
 }
---
+++ test/com/carlosrobertofreire/whiteboard/algorithm/QuickSortTest.java
@@ -0,0 +1,81 @@
+package com.carlosrobertofreire.whiteboard.algorithm;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.carlosrobertofreire.util.ArrayGenerator;
+
+public class QuickSortTest {
+	
+	QuickSort target = null;
+
+	@Before
+	public void setUp() throws Exception {
+		target = new QuickSort();
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void test_sort_inputIsNull() {
+		target.sort(null);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void test_sort_inputIsEmpty() {
+		target.sort(new int[0]);
+	}
+
+	@Test
+	public void test_sort_inputWithOneItem_success() {
+		int[] arrayWithOneItem = new int[] { 5 };
+		Assert.assertEquals(arrayWithOneItem, target.sort(arrayWithOneItem));
+	}
+
+	@Test
+	public void test_sort_unsortedInputWithFiveItems_success() {
+		int[] unsortedArray = new int[] { 7, 4, 10, 3, 16 };
+		int[] expectedSortedArray = new int[] { 3, 4, 7, 10, 16 };
+		int[] sortedArrayByTarget = target.sort(unsortedArray);
+		for (int i = 0; i < sortedArrayByTarget.length; i++) {
+			Assert.assertEquals(expectedSortedArray[i], sortedArrayByTarget[i]);
+		}
+	}
+
+	@Test
+	public void test_sort_unsortedInputWithThirtyItems_success() {
+		testSort(ArrayGenerator.createRandomArrayWithoutDuplicates(30, 300));
+	}
+
+	@Test
+	public void test_sort_unsortedInputWithNinetyItems_success() {
+		testSort(ArrayGenerator.createRandomArrayWithoutDuplicates(90, 300));
+	}
+
+	@Test
+	public void test_sort_unsortedInputWithDuplicatedItems_success() {
+		testSort(ArrayGenerator.createUnsortedArrayWithDuplicatedItems());
+	}
+
+	@Test
+	public void test_sort_sortedInputWithFiveItems_success() {
+		testSort(ArrayGenerator.createSortedFiveItemsArray());
+	}
+
+	@Test
+	public void test_sort_sortedInputWithDuplicatedItems_success() {
+		testSort(ArrayGenerator.createSortedArrayWithDuplicatedItems());
+	}
+
+	private void testSort(int[] array) {
+		int[] expectedSortedArray = Arrays.copyOf(array, array.length);
+		Arrays.sort(expectedSortedArray);
+		int[] sortedArrayByTarget = target.sort(array);
+		for (int i = 0; i < sortedArrayByTarget.length; i++) {
+			Assert.assertEquals(expectedSortedArray[i], sortedArrayByTarget[i]);
+		}
+	}
+
+
+}
