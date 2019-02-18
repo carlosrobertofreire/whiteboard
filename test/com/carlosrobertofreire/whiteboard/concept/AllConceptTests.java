594f08ad9c334912c6994424f7b6f6de7ea9d638
Carlos Roberto Freire <carlosrobertofreire@gmail.com>
2019-02-18 10:07:57 +0000

    Add DynamicProgrammingTest to AllConceptTests.

--- test/com/carlosrobertofreire/whiteboard/concept/AllConceptTests.java
+++ test/com/carlosrobertofreire/whiteboard/concept/AllConceptTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ RecursionTest.class })
+@SuiteClasses({ RecursionTest.class, DynamicProgrammingTest.class })
 public class AllConceptTests {
 
 }
